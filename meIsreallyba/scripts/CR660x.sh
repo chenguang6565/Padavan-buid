2622c2346396a9ff6027a769cb71e763aaf13fca
chenguang6565 <107694341+chenguang6565@users.noreply.github.com>
2022-07-11 19:58:00 +0800

    Update CR660x.sh

--- meIsreallyba/scripts/CR660x.sh
+++ meIsreallyba/scripts/CR660x.sh
@@ -20,20 +20,6 @@ sed -i 's/{ "wan_pppoe_passwd", "" }/{ "wan_pppoe_passwd", "672877" }/g' $defaul
 #echo "打开5G无线桥接"
 #sed -i 's/{ "wl_mode_x", "0" }/{ "wl_mode_x", "4" }/g' $default_path/defaults.c
 
-#CPU超频
-echo "CONFIG_FIRMWARE_INCLUDE_OC=y" >> $config #CPU超频开关
-echo 'CONFIG_FIRMWARE_MT7621_OC="0x312"' >> $config #此处填写对应频率的十六进制：1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
 
-#echo "集成科学插件"
-grep "CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=y" $config
-if [ $? -eq 0 ]; then
-  for MOD in SHADOWSOCKS XRAY TROJAN SSOBFS IPSET EAP_PEAP OPENSSH OPENSSL_EC OPENSSL_EXE CURL; do
-    sed -i "s/CONFIG_FIRMWARE_INCLUDE_${MOD}=n/CONFIG_FIRMWARE_INCLUDE_${MOD}=y/g" $config
-    
-##广告管理##
-echo "CONFIG_FIRMWARE_INCLUDE_ADBYBY=y" >> $.config #adbyby plus+
-
- ##DNS服务##
-echo "CONFIG_FIRMWARE_INCLUDE_SMARTDNS=y" >> $.config #smartdns
 #  done
 #fi
